f35e987d620d865c30a409f71dfd3009681bacfb
Yannick Perret <tchoune@Yannicks-MacBook-Air.local>
2023-03-02 10:50:52 +0100

    fixed height blank

--- src/template/Footer.tsx
+++ src/template/Footer.tsx
@@ -6,52 +6,43 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div className="footer__container">
-                <div className="footer__container--header">
-                    <p>ONE-CONSEILS SARL</p>
+                <p className='footer__container--header'>ONE-CONSEILS SARL</p>
+                <div className='footer__container__item'>
+                    <ul className="footer__container__item--link">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Nos Services</a></li>
+                        <li><a href="#">Fonctionnement</a></li>
+                        <li><a href="#">Philosophie</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
                 </div>
 
                 <div className='footer__container__item'>
-                    <div className="footer__container__item--body">
-                        <ul>
-                            <li><a href="#">Accueil</a></li>
-                            <li><a href="#">Nos Services</a></li>
-                            <li><a href="#">Fonctionnement</a></li>
-                            <li><a href="#">Philosophie</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
+                    <div className="footer__container__item--header">
+                        <p>Accès direct</p>
                     </div>
+                    <ul className="footer__container__item--link">
+                        <li><a href="#">Assurance</a></li>
+                        <li><a href="#">Comptabilité & Administration</a></li>
+                        <li><a href="#">Immobilier</a></li>
+                        <li><a href="#">Informatique</a></li>
+                    </ul>
                 </div>
 
-                <div className='footer__container__two'>
-                    <div className='footer__container__two__item'>
-                        <div className="footer__container__two__item--header">
-                            <p>Accès direct</p>
-                        </div>
-                        <div className="footer__container__two__item--body">
-                            <ul>
-                                <li><a href="#">Assurance</a></li>
-                                <li><a href="#">Comptabilité & Administration</a></li>
-                                <li><a href="#">Immobilier</a></li>
-                                <li><a href="#">Informatique</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className='footer__container__two__item'>
-                        <div className="footer__container__two__item--header">
+                    <div className='footer__container__item'>
+                        <div className="footer__container__item--header">
                             <h2>Newsletter</h2>
                             <button>Inscription &gt;</button>
                         </div>
-                        <div className="footer__container__two__item--body">
+                        <div className="footer__container__item--link">
                             <p>Suivre One-Conseils</p>
-                            <div className="footer__container__two__item--body--image">
+                            <div className="footer__container__item--image">
                                 <img src={linkedin} alt="linkedin" width="100%" height="auto" />
                                 <img src={facebook} alt="facebook" width="100%" height="auto" />
                             </div>
                         </div>
                     </div>
-                </div>
-
+                
 
                 <div className="footer__container--footer">
                     <a href="#">Conditions Générales</a>
